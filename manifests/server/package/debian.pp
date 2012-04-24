538cf0fe934ea61c1f1a524d0666a70186a26f39
Kristian Øllegaard <kristian@kristian.io>
2012-04-24 23:46:40 +0200

    Fix from: http://projects.puppetlabs.com/issues/13043

--- manifests/server/package/debian.pp
+++ manifests/server/package/debian.pp
@@ -6,7 +6,12 @@ class mcollective::server::package::debian {
   }->
   package { 'mcollective':
     ensure  => $mcollective::server::package_version,
+  }->
+  file { 'Debian init script fix': # TODO: Make this parameterized
+	path => '/etc/init.d/mcollective'
+	source => 'puppet:///debian/etc/init.d/mcollective',
   }
+  
 
 }
 
