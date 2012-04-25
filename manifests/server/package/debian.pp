c19d9c4450c39eaf9aa6bbbd3c02ad5540ac4e7b
Kristian Øllegaard <kristian@kristian.io>
2012-04-25 15:07:50 +0200

    Reverted previous commits, this was not the right approach. It was not a debian problem, but a ubuntu problem. It caused if daemonize is set to 1, for ubuntu, set it to 0.

--- manifests/server/package/debian.pp
+++ manifests/server/package/debian.pp
@@ -6,19 +6,6 @@ class mcollective::server::package::debian {
   }->
   package { 'mcollective':
     ensure  => $mcollective::server::package_version,
-  }->
-  file { 'Debian init script fix': # TODO: Make this parameterized
-	path => '/etc/init.d/mcollective',
-	mode => 755,
-	source => 'puppet:///modules/mcollective/debian/etc/init.d/mcollective',
-  }->
-  file { '/etc/init/mcollective.conf':
-    ensure => present
-  }->
-  file_line { 'Debian upstart fix': # TODO: Make this parameterized
-    ensure => present,
-    path => '/etc/init/mcollective.conf',
-    line => 'expect fork',
   }
   
 
